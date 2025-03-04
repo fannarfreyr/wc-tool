56b56fc18ca77c2d8d705c608194d53738d65a12
fannarfreyr <fannarj21@ru.is>
2025-03-04 19:32:51 +0000

    Added line count

--- main.go
+++ main.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
@@ -11,25 +12,51 @@ func countBytes(filePath string) {
 }
 
 func main() {
-	// Define the -c flag
+	// Define flags
 	filePathCountBytes := flag.String("c", "", "")
+	countLines := flag.String("l", "", "")
 
 	// Parse the command-line flags
 	flag.Parse()
 
 	// Check if -c flag was set
-	if *filePathCountBytes == "" {
-		fmt.Println("No file specified. Use -c <file_path>")
+	if *filePathCountBytes == "" && *countLines == "" {
+		fmt.Println("Usage: ")
+		fmt.Println("  -c <file_path> : Get file size in bytes")
+		fmt.Println("  -l <file_path> : Count number of lines in the file")
 		os.Exit(1)
 	}
 
-	// Get file information
-	fileInfo, err := os.Stat(*filePathCountBytes)
-	if err != nil {
-		fmt.Println("Error accessing file:", err)
-		os.Exit(1)
+	// Handle -c flage (byte count)
+	if *filePathCountBytes != "" {
+		fileInfo, err := os.Stat(*filePathCountBytes)
+		if err != nil {
+			fmt.Println("Error accessing file:", err)
+			os.Exit(1)
+		}
+		fmt.Println(fileInfo.Size())
 	}
 
-	// Print the file size in bytes
-	fmt.Println(fileInfo.Size())
+	// Handle -l flag (line count)
+	if *countLines != "" {
+		file, err := os.Open(*countLines)
+		if err != nil {
+			fmt.Println("Error opening file:", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+
+		lineCount := 0
+		scanner := bufio.NewScanner(file)
+
+		for scanner.Scan() {
+			lineCount++
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Println("Error reading file:", err)
+			os.Exit(1)
+		}
+		fmt.Println(lineCount)
+	}
 }
